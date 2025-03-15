3920e6da569ec8442a5d22c5a60558aeaa3ac2e5
Gideon Monterosa <gideon.monterosa@gmail.com>
2025-03-15 13:35:07 +0100

    update keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -6,7 +6,7 @@ map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
---
+
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
@@ -14,4 +14,5 @@ map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 map('n', 'H', '<cmd>bprev<CR>')
 map('n', 'L', '<cmd>bnext<CR>')
 
-map('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
+map('v', '<Tab>', '>gv', { desc = 'increase indent of marked lines' })
+map('v', '<S-Tab>', '<gv', { desc = 'decrease indent of marked lines' })
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,4 +14,30 @@ return {
       { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>', desc = 'Move focus to the right window' },
     },
   },
+
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+    keys = {
+      { '-', '<CMD>Oil --float<CR>', mode = 'n', desc = 'Open parent directory' },
+    },
+    config = function()
+      require('oil').setup()
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'oil',
+        callback = function()
+          -- Map double Escape to close the floating Oil window
+          vim.keymap.set('n', '<Esc><Esc>', '<Cmd>q<CR>', {
+            buffer = true,
+            desc = 'Close Oil floating window',
+          })
+        end,
+      })
+    end,
+  },
 }
